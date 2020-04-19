08bd3f70f7630f953ef0cd1637f829a4907023cb
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-04-19 18:54:15 +0400

    fix: change import for Keyboard.cs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import { get, set } from './utils/session.js';
-import Keyboard from "./Keyboard.js";
+import { get } from './utils/session.js';
+import  Keyboard   from "./Keyboard.js";
 
 const rowsOrder = [
   ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
